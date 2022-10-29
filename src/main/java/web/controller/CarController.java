0e375b32227df2715ff309a0203977fb6ff22ae9
Maksim Goltsov <mgoltsov@bk.ru>
2022-10-29 21:29:53 +0300

    CarController has been commented for a while

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -9,16 +9,16 @@ import java.util.List;
 
 @Controller
 public class CarController {
-
-    @GetMapping(value = "/")
-    public String printCar(ModelMap model) {
-        List<String> messages = new ArrayList<>();
-        messages.add("Hello!");
-        messages.add("I'm Car controller");
-        messages.add("version by oct 22");
-        model.addAttribute("messages", messages);
-        return "index";
-    }
+//
+//    @GetMapping(value = "/")
+//    public String printCar(ModelMap model) {
+//        List<String> messages = new ArrayList<>();
+//        messages.add("Hello!");
+//        messages.add("I'm Car controller");
+//        messages.add("version by oct 22");
+//        model.addAttribute("messages", messages);
+//        return "index";
+//    }
 
 }
 
