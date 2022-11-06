cb758e9e68a62bdc6dbeeda2fc00441b6ddf01e4
Maksim Goltsov <mgoltsov@bk.ru>
2022-11-06 22:49:24 +0300

    Car Service Class initializes listOfCar

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -2,14 +2,20 @@ package web.service;
 
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class CarServiceImpl implements CarService {
     private static List<Car> listOfCar;
 
-    public static void setListOfCar(List<Car> list) {
-        listOfCar = list;
+    public static void setListOfCar() {
+        listOfCar = new ArrayList<>();
+        listOfCar.add(new Car("Toyota", "Trueno", 1999));
+        listOfCar.add(new Car("Nissan", "Sunny", 2001));
+        listOfCar.add(new Car("Subaru", "Impreza", 2007));
+        listOfCar.add(new Car("Lada", "Priora", 2022));
+        listOfCar.add(new Car("Land Rover", "Discovery", 2012));
     }
 
     public static List<Car> getListedCars(int numberOfCar) {
