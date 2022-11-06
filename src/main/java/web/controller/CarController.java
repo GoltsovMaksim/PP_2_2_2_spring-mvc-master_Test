20cbceb23c7b542a7e5dbe2912cbd9f932b73c66
Maksim Goltsov <mgoltsov@bk.ru>
2022-11-06 22:49:24 +0300

    Car Service Class initializes listOfCar

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -7,21 +7,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarServiceImpl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CarController {
 
     @GetMapping(value = "/cars")
-    public String printCar(@RequestParam(value="count",required = false) int a, ModelMap model) {
-        List<Car> tmpList = new ArrayList<>();
-        tmpList.add(new Car("Toyota", "Trueno", 1999));
-        tmpList.add(new Car("Nissan", "Sunny", 2001));
-        tmpList.add(new Car("Subaru", "Impreza", 2007));
-        tmpList.add(new Car("Lada", "Priora", 2022));
-        tmpList.add(new Car("Land Rover", "Discovery", 2012));
-        CarServiceImpl.setListOfCar(tmpList);
+    public String printCar(@RequestParam(value = "count", required = false) int a, ModelMap model) {
+        CarServiceImpl.setListOfCar();
         List<Car> list = CarServiceImpl.getListedCars(a);
         model.addAttribute("listOfCar", list);
         return "cars";
