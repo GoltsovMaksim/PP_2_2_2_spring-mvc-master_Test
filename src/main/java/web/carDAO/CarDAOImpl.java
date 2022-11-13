d255c44c3a80f83c76d167bb777a5c47efe8c69b
Maksim Goltsov <mgoltsov@bk.ru>
2022-11-13 08:59:25 +0300

    Final version 1.0

--- src/main/java/web/carDAO/CarDAOImpl.java
+++ src/main/java/web/carDAO/CarDAOImpl.java
@@ -8,17 +8,15 @@ import java.util.stream.Collectors;
 @Component
 public class CarDAOImpl implements CarDAO{
 
-    private static int CAR_ID;
-
     private final List<Car> listOfCar;
 
     {
         listOfCar = new ArrayList<>();
-        listOfCar.add(new Car(++CAR_ID,"Toyota", "Trueno", 1999));
-        listOfCar.add(new Car(++CAR_ID,"Nissan", "Sunny", 2001));
-        listOfCar.add(new Car(++CAR_ID,"Subaru", "Impreza", 2007));
-        listOfCar.add(new Car(++CAR_ID,"Lada", "Priora", 2022));
-        listOfCar.add(new Car(++CAR_ID,"Land Rover", "Discovery", 2012));
+        listOfCar.add(new Car(1,"Toyota", "Trueno", 1999));
+        listOfCar.add(new Car(2,"Nissan", "Sunny", 2001));
+        listOfCar.add(new Car(3,"Subaru", "Impreza", 2007));
+        listOfCar.add(new Car(4,"Lada", "Priora", 2022));
+        listOfCar.add(new Car(5,"Land Rover", "Discovery", 2012));
     }
     @Override
     public List<Car> getListedCars(int id) {
